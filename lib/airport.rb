e55389e0bdfce1d045edecf402d18276e39bfa89
Katie Koschland <katiekoschland@hotmail.com>
2017-02-23 19:52:23 +0000

    Plane cannot take_off or land during stormy weather

--- lib/airport.rb
+++ lib/airport.rb
@@ -6,12 +6,23 @@ class Airport
   end
 
   def land(plane)
-    raise 'Cannot land plane: airport is full' if @planes.length >= @capacity
+    raise 'Cannot land plane: airport is full' if full?
+    raise 'Cannot land plane: weather is story' if stormy?
     @planes << plane
-  end 
-
-  def take_off(plane)
   end
 
+  def take_off(plane)
+    raise 'Cannot take off: weather is stormy' if stormy?
+  end
+
+private
+
+  def full?
+    @planes.length >= @capacity
+  end
+
+  def stormy?
+  rand(1..8) > 5
+  end
 
 end
