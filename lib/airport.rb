776301641be5d148f20a31dd3cc4d11fa619a12f
Katie Koschland <katiekoschland@hotmail.com>
2017-02-23 19:03:43 +0000

    refactoring of the third user story

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,14 @@
 class Airport
 
-  def land(plane)
+  def initialize(capacity)
+    @capacity = capacity
+    @planes = []
   end
+
+  def land(plane)
+    raise 'Cannot land plane: airport is full' if @planes.length >= @capacity
+    @planes << plane
+  end 
 
   def take_off(plane)
   end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,10 +3,15 @@ require 'plane'
 describe Plane do
 
   it 'so planes land at airports, instructs a plane to land' do
-    airport = Airport.new
+    airport = Airport.new(20)
     plane = Plane.new
-    expect{ airport.land(plane).not_to raise_error}
+    expect{ airport.land(plane) }.not_to raise_error
   end
 
+  it 'so planes take off from airports, instruct a plane to take off' do
+    airport = Airport.new(20)
+    plane = Plane.new
+    expect{ airport.take_off(plane) }.not_to raise_error
+  end
 
 end
