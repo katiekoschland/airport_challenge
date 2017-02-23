958e19cb349db23585059e29344f732f23b3f9ca
Katie Koschland <katiekoschland@hotmail.com>
2017-02-23 19:58:30 +0000

    Fourth user story completed

---
+++ spec/weather_spec.rb
@@ -0,0 +1,16 @@
+#
+# As an air traffic controller
+# To ensure safety
+# I want to prevent takeoff when weather is stormy
+#
+# As an air traffic controller
+# To ensure safety
+# I want to prevent landing when weather is stormy
+
+# it 'does not allow planes to land when stormy' do
+#   airport = Airport.new(20)
+#   plane = Plane.new
+#   allow(airport).to receive(:stormy?).and_return true
+#   expect{ airport.land(plane) }.to raise_error 'Cannot land plane: weather is stormy'
+# end
+# # end
