bbb20c7e5967aa95080fd4ec0c58383d4dce7473
Katie Koschland <katiekoschland@hotmail.com>
2017-02-23 18:03:42 +0000

    refactoring of the first user story

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,8 +6,10 @@
 require 'airport'
 
 describe Airport do
+  subject(:airport){ described_class.new}
+
   it 'instructs a plane to land' do
-    expect(subject).to respond_to(:land).with(1).argument
+    expect(airport).to respond_to(:land).with(1).argument
   end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,5 +2,11 @@ require 'plane'
 
 describe Plane do
 
+  it 'so planes land at airports, instructs a plane to land' do
+    airport = Airport.new
+    plane = Plane.new
+    expect{ airport.land(plane).not_to raise_error}
+  end
+
 
 end
